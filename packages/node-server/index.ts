13511bc4b868c9a7c6dd94219fbb938a5e3f453b
Moorad <moorad2003@gmail.com>
2022-10-05 20:58:22 +0100

    Link sign up form with backend

--- packages/node-server/index.ts
+++ packages/node-server/index.ts
@@ -1,7 +1,22 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 
-app.listen((process.env.PORT || "4000"), () => {
-	console.log(`Server listening on port ${(process.env.PORT || "4000")}`);
+app.use(cors());
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+app.listen((process.env.PORT || '4000'), () => {
+	console.log(`Server listening on port ${(process.env.PORT || '4000')}`);
 });
+
+app.post('/api/register', (req, res) => {
+	console.log(req.body);
+
+	res.json({
+		'message': 'ping'
+	})
+})
