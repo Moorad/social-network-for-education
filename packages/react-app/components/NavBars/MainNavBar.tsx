e0864647885b83b91440c64741104921ae54a0df
Moorad <moorad2003@gmail.com>
2023-02-10 15:39:50 +0000

    UI: implemented a notification menu

--- packages/react-app/components/NavBars/MainNavBar.tsx
+++ packages/react-app/components/NavBars/MainNavBar.tsx
@@ -1,17 +1,32 @@
-import { faCog, faRightFromBracket, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+	faBell,
+	faCog,
+	faMessage,
+	faRightFromBracket,
+	faUser,
+	IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Menu } from '@headlessui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Link from 'next/link';
 import router from 'next/router';
 import { selectAvatar } from '../../redux/userSlice';
 import useLogout from '../../utils/hooks/useLogout';
 import SideNavBar from './SideNavBar';
+import EmptyMessage from '../EmptyMessage';
 
 type propTypes = {
 	active: number;
 	children: JSX.Element;
+};
+
+type Notification = {
+	userAvatar: string;
+	icon: IconDefinition;
+	type: string;
+	text: string;
 };
 
 export default function MainNavBar(props: propTypes) {
@@ -22,18 +37,25 @@ export default function MainNavBar(props: propTypes) {
 				<div className=''>
 					<Link href='/home'>
 						<div className='bg-blue-500 w-20 h-20 flex justify-center align-middle items-center'>
-							<img src='/logos/logo.png' className='m-autoa h-12'></img>
+							<img
+								src='/logos/logo.png'
+								className='m-autoa h-12'
+							></img>
 						</div>
 					</Link>
 				</div>
-				<div className='flex items-center pr-4'>
+				<div className='flex items-center pr-4 gap-5'>
+					<NotificationButton />
 					<TopLeftAccountDropDown avatar={avatar} />
 				</div>
 			</div>
 
 			<div className='flex flex-1 h-0'>
 				<SideNavBar active={props.active} />
-				<div className='flex-1 h-full overflow-y-auto' id='main-section'>
+				<div
+					className='flex-1 h-full overflow-y-auto'
+					id='main-section'
+				>
 					{props.children}
 				</div>
 			</div>
@@ -83,6 +105,98 @@ function TopLeftAccountDropDown(props: { avatar: string }) {
 											{e.name}
 										</div>
 									</Menu.Item>
+								))}
+							</Menu.Items>
+						</div>
+					)}
+				</>
+			)}
+		</Menu>
+	);
+}
+
+function NotificationButton() {
+	const [unreadNotifications, setUnreadNotifications] = useState(true);
+	const [notifications, setNotifications] = useState<Notification[]>([
+		{
+			userAvatar:
+				'http://localhost:4000/resource/70bb12c5-5084-4f73-8302-451a2764e3e2',
+			icon: faMessage,
+			type: 'Message',
+			text: 'Hello world',
+		},
+		{
+			userAvatar:
+				'http://localhost:4000/resource/70bb12c5-5084-4f73-8302-451a2764e3e2',
+			icon: faUser,
+			type: 'Friend Request',
+			text: 'Hello world',
+		},
+	]);
+
+	function clearNotifications() {
+		setUnreadNotifications(false);
+	}
+
+	return (
+		<Menu>
+			{({ open }) => (
+				<>
+					<div className='relative'>
+						<Menu.Button
+							className='hover:bg-gray-200 p-2 rounded-md w-14 h-14'
+							onClick={() => clearNotifications()}
+						>
+							<FontAwesomeIcon
+								className='text-xl text-gray-700'
+								icon={faBell}
+							/>
+						</Menu.Button>
+						{unreadNotifications && (
+							<div className='flex justify-center items-center -bottom-1 -right-1 absolute bg-red-500 text-white w-5 h-5 text-sm rounded-full'>
+								3
+							</div>
+						)}
+					</div>
+					{open && (
+						<div className='absolute right-2 top-[78px] border-gray-300 border p-3 rounded-md mt-3 w-96 z-[100] bg-white cursor-pointer'>
+							<Menu.Items static>
+								<div className='text-gray-900 font-bold mb-3'>
+									Notifications
+								</div>
+								<EmptyMessage
+									message='No recent notifications'
+									value={notifications}
+									background='bg-white'
+								/>
+
+								{notifications.map((n, i) => (
+									<div
+										key={i}
+										className='hover:bg-gray-100 p-2 rounded-md'
+									>
+										<div className='flex'>
+											<div className='relative self-center'>
+												<img
+													className='w-9 ml-1 mr-4 text-gray-700 rounded-full'
+													src={n.userAvatar}
+												/>
+												<FontAwesomeIcon
+													className='absolute text-xs rounded-md bg-blue-500 p-1 right-2 -bottom-1 text-white'
+													icon={n.icon}
+												/>
+											</div>
+
+											<div>
+												<div className='text-sm font-semibold text-gray-700'>
+													{n.type}
+												</div>
+												<div className='text-gray-700'>
+													{n.text}
+												</div>
+											</div>
+										</div>
+									</div>
 								))}
 							</Menu.Items>
 						</div>
