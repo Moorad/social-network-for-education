a33799a252312bddaf77c6908a1afdac63475696
Moorad <moorad2003@gmail.com>
2023-04-28 05:34:22 +0100

    almost finished recommendation system

--- packages/node-server/recommendation/model.py
+++ packages/node-server/recommendation/model.py
@@ -5,68 +5,51 @@ from sklearn.preprocessing import LabelEncoder
 from multiprocessing import Pool, cpu_count
 
 class EASE:
-    def __init__(self):
-        self.user_enc = LabelEncoder()
-        self.item_enc = LabelEncoder()
+	def __init__(self):
+		self.user_enc = LabelEncoder()
+		self.item_enc = LabelEncoder()
 
-    def _get_users_and_items(self, df):
-        users = self.user_enc.fit_transform(df.loc[:, 'user_id'])
-        items = self.item_enc.fit_transform(df.loc[:, 'item_id'])
-        return users, items
+	def _get_users_and_items(self, df):
+		users = self.user_enc.fit_transform(df.loc[:, 'user_id'])
+		items = self.item_enc.fit_transform(df.loc[:, 'item_id'])
+		return users, items
 
-    def fit(self, df, lambda_: float = 0.5, implicit=True):
-        """
-        df: pandas.DataFrame with columns user_id, item_id and (rating)
-        lambda_: l2-regularization term
-        implicit: if True, ratings are ignored and taken as 1, else normalized ratings are used
-        """
-        users, items = self._get_users_and_items(df)
-        values = (
-            np.ones(df.shape[0])
-            if implicit
-            else df['rating'].to_numpy() / df['rating'].max()
-        )
+	def fit(self, df, lamb = 0.5, implicit=True):
+		users, items = self._get_users_and_items(df)
+		values = (
+			np.ones(df.shape[0])
+			if implicit
+			else df['rating'].to_numpy() / df['rating'].max()
+		)
 
-        X = csr_matrix((values, (users, items)))
-        self.X = X
+		X = csr_matrix((values, (users, items)))
+		self.X = X
 
-        G = X.T.dot(X).toarray()
-        diagIndices = np.diag_indices(G.shape[0])
-        G[diagIndices] += lambda_
-        P = np.linalg.inv(G)
-        B = P / (-np.diag(P))
-        B[diagIndices] = 0
+		G = X.T.dot(X).toarray()
+		diagIndices = np.diag_indices(G.shape[0])
+		G[diagIndices] += lamb
+		P = np.linalg.inv(G)
+		B = P / (-np.diag(P))
+		B[diagIndices] = 0
 
-        self.B = B
-        self.pred = X.dot(B)
+		self.B = B
+		self.pred = X.dot(B)
 
-    def predict(self, train, users, items, k):
-        items = self.item_enc.transform(items)
-        dd = train.loc[train.user_id.isin(users)]
-        dd['ci'] = self.item_enc.transform(dd.item_id)
-        dd['cu'] = self.user_enc.transform(dd.user_id)
-        g = dd.groupby('cu')
-        with Pool(cpu_count()) as p:
-            user_preds = p.starmap(
-                self.predict_for_user,
-                [(user, group, self.pred[user, :], items, k) for user, group in g],
-            )
-        df = pd.concat(user_preds)
-        df['item_id'] = self.item_enc.inverse_transform(df['item_id'])
-        df['user_id'] = self.user_enc.inverse_transform(df['user_id'])
-        return df
+	def predict_k(self, u, k):
+		scores = self.X[u, :] @ self.B
+		return np.argsort(-scores)[:, :k]
 
-    @staticmethod
-    def predict_for_user(user, group, pred, items, k):
-        watched = set(group['ci'])
-        candidates = [item for item in items if item not in watched]
-        pred = np.take(pred, candidates)
-        res = np.argpartition(pred, -k)[-k:]
-        r = pd.DataFrame(
-            {
-                "user_id": [user] * len(res),
-                "item_id": np.take(candidates, res),
-                "score": np.take(pred, res),
-            }
-        ).sort_values('score', ascending=False)
-        return r
+	@staticmethod
+	def predict_for_user(user, group, pred, items, k):
+		watched = set(group['ci'])
+		candidates = [item for item in items if item not in watched]
+		pred = np.take(pred, candidates)
+		res = np.argpartition(pred, -k)[-k:]
+		r = pd.DataFrame(
+			{
+				"user_id": [user] * len(res),
+				"item_id": np.take(candidates, res),
+				"score": np.take(pred, res),
+			}
+		).sort_values('score', ascending=False)
+		return r
