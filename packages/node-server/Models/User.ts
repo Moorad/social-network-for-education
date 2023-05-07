e2ae2db53c493d992e4637d79a8c95ba4837c0da
Moorad <moorad2003@gmail.com>
2023-05-07 14:03:17 +0100

    Fixed .env localhost stuff

--- packages/node-server/Models/User.ts
+++ packages/node-server/Models/User.ts
@@ -1,6 +1,11 @@
 import { Schema, model, Types } from 'mongoose';
 import { z } from 'zod';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+console.log(process.env);
+
 const NotificationZod = z.object({
 	type: z.enum(['Message', 'Friend Request']),
 	text: z.string(),
